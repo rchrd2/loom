2a571bebce4af42427d7be7211f0796ca30c5d0a
rcaceres <me@rchrd.net>
2015-01-01 19:23:29 -0800

    update puppet installation. librarian-puppet needs make so we install `build-essentials` from apt-get. tested in ubuntu 14.04

--- loom/puppet.py
+++ loom/puppet.py
@@ -102,6 +102,7 @@ def install():
     with settings(warn_only=True):
         sudo('apt-get -y -q install rubygems')
     sudo('apt-get -y -q install ruby ruby-dev git')
+    sudo('apt-get -y -q install build-essentials')
 
     puppet_version = env.get('loom_puppet_version')
     sudo(_gem_install('puppet', version=puppet_version))
